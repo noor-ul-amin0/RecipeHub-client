8191a3af8f7dc61b3470db625f8de197377fa6ad
Noor Ul Amin <noorawan444@gmail.com>
2023-05-24 18:17:38 +0500

    add login api route

---
+++ app/api/auth/login/route.js
@@ -0,0 +1,28 @@
+import { verifyPassword } from "@/lib/auth";
+import { connectDatabase } from "@/lib/db";
+import User from "@/models/user";
+import { Responses } from "@/utils/API_Responses";
+
+export const POST = async (request) => {
+  const { email, password } = await request.json();
+  if (!email || !password)
+    return Responses._400({
+      message: "Missing email or password",
+      success: false,
+    });
+  try {
+    await connectDatabase();
+    const user = await User.findOne({ email }).select(
+      "+password -createdAt -updatedAt -__v"
+    );
+    if (!user || !(await verifyPassword(password, user.password))) {
+      return Responses._400({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+    return Responses._200({ success: true, data: user });
+  } catch (error) {
+    return Responses._500({ success: false, message: error.message });
+  }
+};
