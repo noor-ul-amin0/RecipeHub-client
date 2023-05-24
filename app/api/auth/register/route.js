e9ac118d702ba899428cc88ca8d927b8d0cd5ff0
Noor Ul Amin <noorawan444@gmail.com>
2023-05-24 18:18:55 +0500

    use API Response utility func in register route

--- app/api/auth/register/route.js
+++ app/api/auth/register/route.js
@@ -1,23 +1,32 @@
+import { hashPassword } from "@/lib/auth";
 import { connectDatabase } from "@/lib/db";
 import User from "@/models/user";
+import { Responses } from "@/utils/API_Responses";
 
 export const POST = async (request) => {
   const { email, password, fullName } = await request.json();
   if (!email || !password || !fullName) {
-    return new Response("Missing fields", { status: 400 });
+    return Responses._400({ success: false, message: "Missing fields" });
   }
   try {
     await connectDatabase();
     const user = await User.findOne({ email });
     if (user) {
-      return new Response("Email already exists!", { status: 400 });
+      return Responses._400({
+        success: false,
+        message: "Email already exists!",
+      });
     }
+    const hashedPassword = await hashPassword(password);
     await User.create({
       email,
-      password,
+      password: hashedPassword,
       fullName,
     });
-    return new Response(null, { status: 201 });
+    return Responses._201({
+      success: true,
+      message: "User created successfully",
+    });
   } catch (error) {
     return new Response("Failed to register user" + error, { status: 500 });
   }
