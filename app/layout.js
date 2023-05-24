a3d910ab4b582d4f4d43e2361e3faaebdb565949
Noor Ul Amin <noorawan444@gmail.com>
2023-05-24 18:16:22 +0500

    update Navbar comp path in layout

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-import Navbar from "@/components/Navbar";
+import Navbar from "@/components/common/stateless/Navbar";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NextAuthSessionProvider from "@/components/NextAuthSessionProvider";
