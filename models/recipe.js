c5de0e6298396fc9617d7663f2f202a42e3d7aeb
Noor Ul Amin <noorawan444@gmail.com>
2023-05-24 18:23:47 +0500

    Add Recipe model

---
+++ models/recipe.js
@@ -0,0 +1,36 @@
+import { Schema, model, models } from "mongoose";
+
+const recipeSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    ingredients: {
+      type: [String],
+      required: true,
+    },
+    instructions: {
+      type: [String],
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    creator: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Recipe = models?.Recipe || model("Recipe", recipeSchema);
+
+export default Recipe;
