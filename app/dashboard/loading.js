2650a5c48832eb136200645ab50fa481271189bc
Noor Ul Amin <noorawan444@gmail.com>
2023-05-25 11:16:28 +0500

    add a user dashboard page to manage his/her recipes

---
+++ app/dashboard/loading.js
@@ -0,0 +1,6 @@
+"use client";
+import Loader from "@/components/common/stateless/Loader";
+
+export default function Loading() {
+  return <Loader />;
+}
--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,5 +1,55 @@
-import React from "react";
+"use client";
+import { getUserRecipes } from "@/lib/recipes";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Typography,
+} from "@mui/material";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
 
-export default function Dashboard() {
-  return <div>Dashboard</div>;
+export default async function Dashboard() {
+  const [recipes, setRecipes] = useState([]);
+  const { data: session } = useSession();
+  useEffect(() => {
+    if (session) {
+      getUserRecipes(session.user._id)
+        .then((recipes) => {
+          setRecipes(recipes);
+        })
+        .catch((error) => alert(error.message));
+    }
+  }, [session]);
+
+  return recipes.length > 0 ? (
+    recipes.map((recipe) => (
+      <Card key={recipe._id}>
+        <CardContent>
+          <Typography variant="h5" align="center">
+            {recipe.name}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button variant="outlined" color="primary">
+            View
+          </Button>
+        </CardActions>
+      </Card>
+    ))
+  ) : (
+    <Card>
+      <CardContent>
+        <Typography variant="h5" align="center">
+          No Recipes exist
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button onClick={() => {}} variant="outlined">
+          Add a Recipe
+        </Button>
+      </CardActions>
+    </Card>
+  );
 }
