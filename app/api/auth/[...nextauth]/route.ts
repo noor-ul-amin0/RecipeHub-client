fbb4d55cf339cdbaa8aded7493ac644a19888d93
Noor Ul Amin <noorawan444@gmail.com>
2023-05-31 13:30:10 +0500

    convert ...nextauth file to ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -7,12 +7,7 @@ const handler = NextAuth({
   providers: [
     CredentialsProvider({
       type: "credentials",
-      // The name to display on the sign in form (e.g. 'Sign in with...')
       name: "Credentials",
-      // The credentials is used to generate a suitable form on the sign in page.
-      // You can specify whatever fields you are expecting to be submitted.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
         username: {
           label: "Email",
@@ -27,7 +22,7 @@ const handler = NextAuth({
       },
       async authorize(credentials) {
         const response = await fetch(
-          `${process.env.NEXTAUTH_URL}/api/auth/login`,
+          `${process.env.NEXTAUTH_URL}/api/auth/signin`,
           {
             method: "POST",
             body: JSON.stringify({
