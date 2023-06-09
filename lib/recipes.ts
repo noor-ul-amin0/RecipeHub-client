3def9562d3580f9b0009dc3bf752412915879648
Noor Ul Amin <noorawan444@gmail.com>
2023-06-09 23:04:05 +0500

    add addRecipe func

--- lib/recipes.ts
+++ lib/recipes.ts
@@ -1,5 +1,19 @@
 export const getAllRecipes = async () => {
-  const response = await fetch(`/api/recipes`);
+  const response = await fetch(`http://localhost:8080/api/recipes`, {
+    cache: "no-store",
+  });
+  const { data, success, message } = await response.json();
+  if (!response.ok || !success) {
+    throw new Error(message);
+  }
+  return data;
+};
+export const addRecipe = async (recipe: IRecipeFormData) => {
+  const response = await fetch(`http://localhost:8080/api/recipes`, {
+    body: JSON.stringify(recipe),
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+  });
   const { data, success, message } = await response.json();
   if (!response.ok || !success) {
     throw new Error(message);
