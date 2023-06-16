9aff4b3cb044aaf5bce2f26835e1e79a3f9cfe75
Noor Ul Amin <noorawan444@gmail.com>
2023-06-16 15:09:43 +0500

    remove styling in AddRecipe btn

--- components/AddRecipe/AddRecipeButton.tsx
+++ components/AddRecipe/AddRecipeButton.tsx
@@ -1,32 +1,19 @@
 "use client";
-import { Box, Button, styled } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import { FC } from "react";
 
-const BoxStyle = styled(Box)(({ theme }) => ({
-  position: "fixed",
-  bottom: 30,
-  right: 120,
-  [theme.breakpoints.down("md")]: {
-    bottom: 20,
-    right: 8,
-  },
-  [theme.breakpoints.down("sm")]: {
-    bottom: 6,
-    right: 15,
-  },
-}));
 interface IAddRecipeButton {
   onClick: () => void;
 }
 const AddRecipeButton: FC<IAddRecipeButton> = ({ onClick }) => {
   return (
     <>
-      <BoxStyle>
+      <Box>
         <Button onClick={onClick} variant="contained" startIcon={<AddIcon />}>
           Add Recipe
         </Button>
-      </BoxStyle>
+      </Box>
     </>
   );
 };
