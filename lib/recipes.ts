d3b5c451738c31e2a83941da6fba9a63c7a5adaa
Noor Ul Amin <noorawan444@gmail.com>
2023-06-16 15:04:32 +0500

    add jwt in recipes apis

--- lib/recipes.ts
+++ lib/recipes.ts
@@ -1,5 +1,10 @@
-export const getAllRecipes = async () => {
+export const getAllRecipes = async (accessToken: string | any) => {
+  const bearerToken = accessToken && `Bearer ${accessToken}`;
   const response = await fetch(`http://localhost:8080/api/recipes`, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: bearerToken,
+    },
     cache: "no-store",
   });
   const { data, success, message } = await response.json();
@@ -8,11 +13,17 @@ export const getAllRecipes = async () => {
   }
   return data;
 };
-export const addRecipe = async (recipe: IRecipeFormData) => {
+export const addRecipe = async (
+  recipe: IRecipeFormData,
+  token: string | any
+) => {
   const response = await fetch(`http://localhost:8080/api/recipes`, {
     body: JSON.stringify(recipe),
     method: "POST",
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
   });
   const { data, success, message } = await response.json();
   if (!response.ok || !success) {
