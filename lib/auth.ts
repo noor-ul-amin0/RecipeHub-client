98d48f8c8f100476868d6fea5c0304f6443fe818
Noor Ul Amin <noorawan444@gmail.com>
2023-06-16 15:04:48 +0500

    remove unnecessary code in auth lib

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,10 +1,9 @@
-import User from "@/models/user";
-import { hash, compare } from "bcryptjs";
-import { connectDatabase } from "./db";
-import isEmpty from "lodash/isEmpty";
-
-export async function signUp(fullName:string, email:string, password:string) {
-  const response = await fetch("/api/auth/signup", {
+export async function signUp(
+  fullName: string,
+  email: string,
+  password: string
+) {
+  const response = await fetch(`http://localhost:8080/api/users/signup`, {
     method: "POST",
     body: JSON.stringify({ fullName, email, password }),
     headers: {
@@ -16,21 +15,4 @@ export async function signUp(fullName:string, email:string, password:string) {
     throw new Error(data.message);
   }
   return response;
-}
-export async function findUser(fields = {}) {
-  if (isEmpty(fields)) throw new Error("Something went wrong");
-  await connectDatabase();
-  const user = await User.findOne(fields);
-  if (!user) throw new Error("User not found");
-  return user;
-}
-
-export async function hashPassword(password:string): Promise<string>{
-  const hashedPassword = await hash(password, 12);
-  return hashedPassword;
-}
-
-export async function verifyPassword(password:string, hashedPassword:string): Promise<boolean> {
-  const isValid = await compare(password, hashedPassword);
-  return isValid;
 }
