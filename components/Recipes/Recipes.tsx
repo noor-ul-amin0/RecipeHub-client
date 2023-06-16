80039964e3962c5b9f7f317b2fc3baa1be2fa189
Noor Ul Amin <noorawan444@gmail.com>
2023-06-16 15:02:21 +0500

    add authentication in Recipes SC

--- components/Recipes/Recipes.tsx
+++ components/Recipes/Recipes.tsx
@@ -1,18 +1,23 @@
 import { getAllRecipes } from "@/lib/recipes";
 import RecipeCard from "../RecipeCard/RecipeCard";
 import styles from "./Recipes.module.scss";
+import AddRecipe from "../AddRecipe/AddRecipe";
+import { getServerSession } from "next-auth";
+import { nextAuthOptions } from "@/app/api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
 export default async function Recipes() {
-  const recipes: IRecipe[] = await getAllRecipes();
+  const session = await getServerSession(nextAuthOptions);
+  if (!session) redirect("/signin");
+  const recipes: IRecipe[] = await getAllRecipes(session?.user.accessToken);
   return (
     <div className={styles.container}>
+      <div className={styles.add_button_container}>
+        <AddRecipe />
+      </div>
       <div className={styles.row}>
         {recipes.map((recipe) => (
-          <RecipeCard
-            key={recipe.id}
-            title={recipe.title}
-            description={recipe.description}
-          />
+          <RecipeCard key={recipe.id} {...recipe} />
         ))}
       </div>
     </div>
