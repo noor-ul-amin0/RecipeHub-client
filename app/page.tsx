22abefd456472115dd9a30de0ee5182e1feb9e54
Noor Ul Amin <noorawan444@gmail.com>
2023-06-16 14:59:11 +0500

    remove AddRecipe component from home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import AddRecipe from "@/components/AddRecipe/AddRecipe";
 import Recipes from "@/components/Recipes/Recipes";
 
 export default function Home() {
   return (
     <>
       <Recipes />
-      <AddRecipe />
     </>
   );
 }
