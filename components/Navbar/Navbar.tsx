c5738ef0b23699d13ef55b90e418b09b932fa92a
Noor Ul Amin <noorawan444@gmail.com>
2023-06-16 15:08:46 +0500

    pass down session to AuthButtons

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,26 +1,29 @@
 "use client";
-import * as React from "react";
-import { AppBar, Grid, Toolbar } from "@mui/material";
+import { AppBar, Container, Grid, Toolbar } from "@mui/material";
 import { useSession } from "next-auth/react";
 import AuthButtons from "./AuthButtons";
+import SearchBox from "./SearchBox";
 
 const Navbar = () => {
-  const { status } = useSession();
+  const { status, data: session } = useSession();
   return (
-    <AppBar position="static" color="transparent">
-      <Toolbar>
-        {status !== "loading" && (
-          <Grid
-            mr={20}
-            container
-            direction="row"
-            justifyContent="end"
-            alignItems="center"
-          >
-            <AuthButtons />
-          </Grid>
-        )}
-      </Toolbar>
+    <AppBar position="sticky">
+      <Container maxWidth="xl">
+        <Toolbar>
+          <SearchBox />
+          {status !== "loading" && (
+            <Grid
+              mr={20}
+              container
+              direction="row"
+              justifyContent="end"
+              alignItems="center"
+            >
+              <AuthButtons session={session} />
+            </Grid>
+          )}
+        </Toolbar>
+      </Container>
     </AppBar>
   );
 };
