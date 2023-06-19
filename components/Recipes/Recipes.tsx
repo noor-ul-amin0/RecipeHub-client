a763a531d2b75fbdcb27bc3d31f784e0925962b1
Noor Ul Amin <noorawan444@gmail.com>
2023-06-19 22:13:35 +0500

    convert Recipes into CC

--- components/Recipes/Recipes.tsx
+++ components/Recipes/Recipes.tsx
@@ -1,15 +1,9 @@
-import { getAllRecipes } from "@/lib/recipes";
 import RecipeCard from "../RecipeCard/RecipeCard";
 import styles from "./Recipes.module.scss";
 import AddRecipe from "../AddRecipe/AddRecipe";
-import { getServerSession } from "next-auth";
-import { nextAuthOptions } from "@/app/api/auth/[...nextauth]/route";
-import { redirect } from "next/navigation";
+import { FC } from "react";
 
-export default async function Recipes() {
-  const session = await getServerSession(nextAuthOptions);
-  if (!session) redirect("/signin");
-  const recipes: IRecipe[] = await getAllRecipes(session?.user.accessToken);
+const Recipes: FC<{ recipes: IRecipe[] }> = ({ recipes = [] }) => {
   return (
     <div className={styles.container}>
       <div className={styles.add_button_container}>
@@ -22,4 +16,5 @@ export default async function Recipes() {
       </div>
     </div>
   );
-}
+};
+export default Recipes;
