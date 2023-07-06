a07513245e6bd17d2c0753df3c72615b13e08d91
Noor Ul Amin <noorawan444@gmail.com>
2023-07-06 16:50:06 +0500

    Add getRecipeBySlug lib

--- lib/recipes.ts
+++ lib/recipes.ts
@@ -1,3 +1,21 @@
+export const getRecipeBySlug = async (
+  accessToken: string | any,
+  slug: string
+) => {
+  const getRecipeURL = `http://localhost:8080/api/recipes/${slug}`;
+  const bearerToken = accessToken && `Bearer ${accessToken}`;
+  const response = await fetch(getRecipeURL, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: bearerToken,
+    },
+  });
+  const { data, success, message } = await response.json();
+  if (!response.ok || !success) {
+    throw new Error(message);
+  }
+  return data;
+};
 export const getAllRecipes = async (
   accessToken: string | any,
   searchTerm?: string | any
